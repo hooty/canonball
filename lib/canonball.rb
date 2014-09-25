8c2d00f13fb3d5e080689ba9cda49986782c6409
Jon Christopher <jon@rubbertreestudios.com>
2014-09-25 12:40:02 -0700

    c library locaiton

--- lib/canonball.rb
+++ lib/canonball.rb
@@ -3,7 +3,7 @@ require "canonball/version"
 
 module Canonball
   extend FFI::Library
-  ffi_lib "./lib/canonball/canonball.so"
+  ffi_lib "#{File.expand_path(File.dirname(__FILE__))}/canonball/canonball.so"
   attach_function :take_picture, [ :int ], :int
   attach_function :download_files, [ :int, :string], :int
 end
