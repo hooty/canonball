ce6bf12924a72503c5807a873b474849b95bb393
Jon Christopher <jon@rubbertreestudios.com>
2014-09-26 09:43:11 -0700

    output path

--- lib/canonball.rb
+++ lib/canonball.rb
@@ -4,7 +4,7 @@ require "canonball/version"
 
 module Canonball
   extend FFI::Library
-  ffi_lib "#{File.expand_path(File.dirname(__FILE__))}/canonball/canonball.so"
+  ffi_lib "#{File.expand_path(File.dirname(__FILE__))}/canonball/canonball.so".force_encoding("utf-8")
   attach_function :take_picture, [ :int ], :int
   attach_function :download_files, [ :int, :string], :int
 end
