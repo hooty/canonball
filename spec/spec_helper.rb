71436a28e83d1987a91861862b138e4574706253
Jon Christopher <jon@rubbertreestudios.com>
2014-10-04 23:59:50 -0700

    random test order rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 require "canonball"
+
+RSpec.configure do |config|
+  config.order = "random"
+  Kernel.srand config.seed
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+  end
+end
+
